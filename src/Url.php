f58087f276d45bdcd4343fa7ae6c54e8bb6cefec
Dave Hourn <dave@dthdesign.com.au>
2023-09-12 15:43:47 +0100

    Add constructor and change static functions

--- src/Url.php
+++ src/Url.php
@@ -7,11 +7,24 @@ namespace mrmojorising\ImageProxy;
  */
 class Url
 {
+    /** @var string */
+    public string $imgProxyHost = 'http://localhost:8080'; // use imgproxy localhost default
+
+    /**
+     * @param string|null $imgProxyHost
+     */
+    public function __construct(string $imgProxyHost = null)
+    {
+        if ($imgProxyHost !== null) {
+            $this->imgProxyHost = $imgProxyHost;
+        }
+    }
+
     /**
      * @param string $imgUrl
      * @return string
      */
-    public static function generate(string $imgUrl): string
+    public function generate(string $imgUrl): string
     {
         return $imgUrl;
     }
@@ -20,7 +33,7 @@ class Url
      * @param array $imgUrls
      * @return array
      */
-    public static function generateBatch(array $imgUrls): array
+    public function generateBatch(array $imgUrls): array
     {
         $imgProxyUrls = [];
 
